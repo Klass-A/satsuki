7f7a8f873c9938f007ee01c0e88c107206830aab
Otman Zaib <otman.zaib@gmail.com>
2018-12-18 16:49:16 +0100

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,9 @@ $(window).on('load', function() { // makes sure the whole site is loaded
 	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
 	$('body').delay(350).css({'overflow':'visible'});
 })
+
+
+
+// Copyrights Get Current Year
+var year = new Date().getFullYear();
+$('.fullYear').html(year)
