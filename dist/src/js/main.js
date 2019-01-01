00637a44264c87ec7eca4b239e1a4f1c0d452c3b
Otman Zaib <otman.zaib@gmail.com>
2019-01-01 18:46:27 +0100

    video player

--- dist/src/js/main.js
+++ dist/src/js/main.js
@@ -1,10 +1,10 @@
 
-$(window).on('load', function() { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow':'visible'});
-})
-
+// Preloader
+// $(window).on('load', function() { // makes sure the whole site is loaded
+// 	$('#status').fadeOut(); // will first fade out the loading animation
+// 	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+// 	$('body').delay(350).css({'overflow':'visible'});
+// })
 
 
 // Copyrights Get Current Year
@@ -12,6 +12,11 @@ var year = new Date().getFullYear();
 $('.fullYear').html(year)
 
 
-$('.categories-icons').on('click', function(){
-	$(this).parent().addClass('active-cat');
+
+// Video player popup
+$('.player-inner').on('click', function(){
+	$('#video-popup').addClass('hide-player');
+})
+$('.close-icon').on('click', function(){
+	$('#video-popup').removeClass('hide-player');
 })
