2bf1e84263aab537156b82d95b0ad811e9ec0403
Otman Zaib <otman.zaib@gmail.com>
2019-01-01 18:47:22 +0100

    Updates

---
+++ src/js/categories.js
@@ -0,0 +1,23 @@
+
+// Categories Active
+$('.categories-icons').on('click', function(){
+	$(this).parent().addClass('active-cat');
+})
+
+if($('#category-boxes .box').classList.contains(let)){
+	$('#category-boxes .box').css('display', 'none');
+}
+
+// Categories Classes
+// $('#category-boxes .box').each(function(){
+// 	$('#category-boxes .box').addClass('all');
+// })
+
+
+/* 
+
+1- On "X-caterogy" icons click.
+2- Only display boxes with "X-caterogy" class
+
+
+*/
